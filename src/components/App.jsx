8b0814f42fb84f12b093917fd3d521346625ed7c
Vijay Kumar Gupta <guptavinay225@gmail.com>
2022-07-16 17:26:09 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+
+function createCard(contactOne){
+  return(
+    <Card 
+    key ={contactOne.id}
+    name={contactOne.name}
+    img={contactOne.imgURL}
+    tel={contactOne.phone}
+    tel={contactOne.email}
+    />
+  );
+}
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+     {contacts.map(createCard)}
+     
+    </div>
+  );
+}
+
+export default App;
